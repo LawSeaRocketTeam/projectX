a4a550353ce0613a5ddf76bc8f4bd2790be74732
010513 <stargjs@foxmail.com>
2019-08-19 17:00:55 +0800

    222222

--- assets/Script/Data/DataMgr.js
+++ assets/Script/Data/DataMgr.js
@@ -3,6 +3,8 @@
 var encrypt = require("encryptjs");
 var secretkey = "project_x_20190809"
 var OP_SETTING_NAME = "OP_SETTING_NAME"
+var GUAN_QIA_DATA_FILENAME = "Resource/Json/chooseData.json"
+
 
 cc.Class({
     extends: cc.Component,
@@ -16,6 +18,11 @@ cc.Class({
             this.opSetting.op = opSettingData.op;
             this.opSetting.sensi = opSettingData.sensi;
         }
+
+        //从文件读取关卡数据
+        cc.loader.loadRes("Resource/Json/chooseData",function(err,jsonAsset){
+            this.guanQiaData = jsonAsset.json;
+        })
     },
 
     //存储操作模式
@@ -26,5 +33,14 @@ cc.Class({
         let encryData = encrypt.encrypt(jsonData,secretkey,256);
         cc.sys.localStorage.setItem(OP_SETTING_NAME,encryData);
     },
+
+    //存储关卡数据到文件
+    saveGuanQiaDataToFile(){
+        if(cc.sys.isNative) {
+            //cc.log("Path:"+jsb.fileUtils.getWritablePath());
+            jsb.fileUtils.writeToFile(this.guanQiaData,jsb.fileUtils.getWritablePath() + GUAN_QIA_DATA_FILENAME);
+            //cc.log("fullPathForFilename:"+jsb.fileUtils.fullPathForFilename("resources/data.json"));
+         }
+    }
 
 });
