bb07171f6daeda8f87365c77ec577c3e64bebe21
010513 <stargjs@foxmail.com>
2019-08-19 21:13:10 +0800

    11111

---
+++ assets/resources/i18n/en.js
@@ -0,0 +1,22 @@
+'use strict';
+
+if (!window.i18n) {
+    window.i18n = {};
+}
+
+if (!window.i18n.languages) {
+    window.i18n.languages = {};
+}
+
+window.i18n.languages['en'] = {
+    // write your key value pairs here
+    "hit_rate" : "Hit Rate:",
+    "sensitivity" : "Sensitivity",
+    "left_hand" : "LH quasi-star",
+    "right_hand" : "RH quasi-star",
+    "change" : "Change",
+    "op_mode" : "Operational Mode",
+    "lv_mode" : "Level Mode",
+    "el_mode" : "Endless Mode",
+
+};
